3fbff2f1f8b7a806b1e647f884a5f04987141a27
MAC <mac@Dricks-Boy.local>
2024-05-26 17:29:42 +0100

    auth zero updated

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-m2f3ppmzvnre585o.us.auth0.com"
       clientId="ihSZBYYkLrkknMZj9UiZGB4CRRaN0MJK"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173",
+        redirect_uri: "https://backend-real-estate-six.vercel.app",
       }}
       audience='this is the unique identifier'
       scope="openid profile email"
