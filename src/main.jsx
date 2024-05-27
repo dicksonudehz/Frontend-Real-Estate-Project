2a89dcd245dcfae04cc96e328e85cfc4a46a313d
MAC <mac@Dricks-Boy.local>
2024-05-27 23:04:33 +0100

    redirect_url update

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-m2f3ppmzvnre585o.us.auth0.com"
       clientId="ihSZBYYkLrkknMZj9UiZGB4CRRaN0MJK"
       authorizationParams={{
-        redirect_uri: "https://backend-real-estate-six.vercel.app/",
+        redirect_uri: "https://frontend-real-estate-project.vercel.app/",
       }}
       audience='this is the unique identifier'
       scope="openid profile email"
